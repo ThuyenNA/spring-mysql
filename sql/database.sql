9ca4576d2e51241fd43e5e13a74130078a3f4293
ThuyenNA <thuyenna@vietis.com.vn>
2021-09-08 11:53:28 +0700

    Second commit

--- sql/database.sql
+++ sql/database.sql
@@ -15,27 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `hibernate_sequence`
---
-
-DROP TABLE IF EXISTS `hibernate_sequence`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hibernate_sequence` (
-    `next_val` bigint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hibernate_sequence`
---
-
-LOCK TABLES `hibernate_sequence` WRITE;
-/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (3);
-/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ticket`
--- src/main/java/com/javatechie/spring/mysql/api/controller/TicketController.java
+++ src/main/java/com/javatechie/spring/mysql/api/controller/TicketController.java
@@ -1,10 +1,18 @@
 package com.javatechie.spring.mysql.api.controller;
 
-import com.javatechie.spring.mysql.api.dao.TicketDao;
+
 import com.javatechie.spring.mysql.api.model.Ticket;
 import com.javatechie.spring.mysql.api.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -18,20 +26,23 @@ public class TicketController {
         return ticketService.getTickets();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/tickets")
+    @PostMapping(value = "/ticket")
     public void add(@RequestBody Ticket ticket){
         ticketService.addTicket(ticket);
     }
 
-    @RequestMapping(value = "/students/{id}", method = RequestMethod.GET)
+
+    @GetMapping(value = "/tickets/{id}")
     public Ticket getATicket(@PathVariable Integer id){
         return ticketService.getATicket(id);
     }
-    @RequestMapping(value =  "/students/{id}", method = RequestMethod.DELETE)
+
+    @DeleteMapping(value =  "/tickets/{id}")
     public  void deleteTicket(@PathVariable Integer id){
         ticketService.deleteTicket(id);
     }
-    @RequestMapping(value =  "/students/{id}", method = RequestMethod.DELETE)
+
+    @PutMapping("/tickets/{id}")
     public  void updateTicket(@PathVariable Integer id, @RequestBody Ticket ticket){
         ticketService.updateTicket(id,ticket);
     }
